26390a0269fc7b4f15e5d5a269a13737f7b2ccd3
atata174 <razhapov_albert@mail.ru>
2021-04-02 18:17:11 +0300

    Initial Commit

---
+++ MyDataFlow/MyDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDataFlowApp.swift
+//  MyDataFlow
+//
+//  Created by PenguinRaja on 02.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
